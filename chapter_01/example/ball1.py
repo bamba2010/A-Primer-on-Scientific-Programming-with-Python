91e36ee17d1c0c36d0aefb9bcc1873fb18749ba4
Bamba <bamba0183@gmail.com>
2019-06-10 10:09:20 -0500

    Add examples of chapter 1.

---
+++ chapter_01/example/ball1.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2019 Cheikh Bamba
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+print(5*0.6 - 0.5*9.81*0.6**2)
---
+++ chapter_01/example/ball6.py
@@ -0,0 +1,6 @@
+v0= 5 # initial vlocity
+g = 9.81 # acceleration of gravity
+t = 0.6  # time
+y = v0*t-0.5*g*t**2 # vertical position
+print (" At t=%f s, a ball with initial velocity v0 =%.3E m/s is located at the heigh %.2f m. "% (t,v0,y))
+
