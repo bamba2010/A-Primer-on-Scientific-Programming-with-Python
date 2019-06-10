940fb05b6446598aad47b5bbcb864b7608a1d4bb
Bamba <bamba0183@gmail.com>
2019-06-10 10:18:06 -0500

    Add copyright.

--- chapter_01/example/ball3.py
+++ chapter_01/example/ball3.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2019 Cheikh Bamba
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 initial_velocity= 5
 acceleration_of_gravity= 9.81
 TIME= 0.6
