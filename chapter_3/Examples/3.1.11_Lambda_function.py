9e579e42b1c3753c971f2892bb1447b371b65107
Bamba <bamba0183@gmail.com>
2019-06-19 18:51:51 -0500

    Adding examples of chapter 3

---
+++ chapter_3/Examples/3.1.11_Lambda_function.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2019 Cheick Bamba
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+def f(x):
+    return  x**2 +4
+
+"""can be rewritten as """
+g = lambda x: x**2 +4
+print(g(5))
+t = f(4)
+print(t)
